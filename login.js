e0fd5e46e28b3d266fd48ac218daf132d6a4f487
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2018-04-25 12:56:17 +0600

    Call api to get CMP endpoint

--- login.js
+++ login.js
@@ -2,7 +2,10 @@ const request = require('request-promise'),
     tough = require('tough-cookie');
 
 
-const LOGIN_URL = 'https://accounts.newscred.com/login';
+const SSO_URL = 'https://accounts.newscred.com',
+    CMP_HOST = 'https://cmp.newscred.com',
+    CMP_HOME = `${CMP_HOST}/cloud/home`,
+    LOGIN_URL = `${SSO_URL}/login`;
 
 login('ashrafuzzaman@newscred.com', 'qweqwe');
 
@@ -14,28 +17,37 @@ document.querySelector('#login-form').onsubmit = function (e) {
     login(email, password);
 };
 
-
 function login(email, password) {
     let req = request.defaults({ jar: true });
-    req.get(LOGIN_URL)
+    return req.get(LOGIN_URL)
         .then(response => {
             let loginDom = new DOMParser().parseFromString(response, "text/html");
             return loginDom.querySelector('input[name=_csrf]').value;
         })
         .then(csrf => {
             return req.post(LOGIN_URL, {
+                followRedirect: true,
                 form: {
                     email: email,
                     password: password,
                     _csrf: csrf
                 }
             }).catch((err) => {
-                console.log('err', err);
-                return null;
+                // TODO: Need to handle invalid login where status != 302
+                return req.get(SSO_URL);
             })
         })
         .then(() => {
-            req.get('https://cmp.newscred.com/api/users', { json: true })
+            return req.get(CMP_HOME)
+                .then((response) => {
+                    var csrfTokenRegexp = /window\.csrfToken\s=\s"(.+)";/g;
+                    var match = csrfTokenRegexp.exec(response);
+                    csrfToken = match[1];
+                    return csrfToken;
+                })
+        }).then((csrfToken) => {
+            let cmcReq = req.defaults({ json: true, headers: { 'X-CSRF-TOKEN': csrfToken } });
+            return cmcReq.get('https://cmp.newscred.com/api/users')
                 .then((users) => {
                     console.log('users', users);
                 })
